f731cc290bbf41dc60696292603fdd0bbfa5fce0
Tomás Ruiz <tomasruizr@gmail.com>
2021-03-18 18:27:53 -0300

    [add] fix and Curry maybeToAsync type

--- Async/maybeToAsync.d.ts
+++ Async/maybeToAsync.d.ts
@@ -6,7 +6,9 @@ import Async from './Async';
  * maybeToAsync :: e -> Maybe a -> Async e a
  * maybeToAsync :: e -> (a -> Maybe b) -> a -> Async e b
  */
-declare function maybeToAsync(val: Maybe): Async;
-declare function maybeToAsync(fn: UnaryFunction<Maybe>): UnaryFunction<Async>;
+declare function maybeToAsync(errVal: any): (maybe: Maybe) => Async;
+declare function maybeToAsync(errVal: any, maybe: Maybe): Async;
+declare function maybeToAsync(fn: UnaryFunction<Maybe>): (maybe: Maybe) => UnaryFunction<Async>;
+declare function maybeToAsync(fn: UnaryFunction<Maybe>, maybe: Maybe): UnaryFunction<Async>;
 
 export default maybeToAsync;
