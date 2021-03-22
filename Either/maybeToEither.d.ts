d02cc80eea1040c5a58bbf5974fa4720739cd887
Tomás Ruiz <tomasruizr@gmail.com>
2021-03-21 23:31:18 -0300

    [fix] MaybeToEither types

--- Either/maybeToEither.d.ts
+++ Either/maybeToEither.d.ts
@@ -6,7 +6,9 @@ import Either from './Either';
  * maybeToEither :: c -> Maybe a -> Either c a
  * maybeToEither :: c -> (a -> Maybe b) -> a -> Either c a
  */
-declare function maybeToEither(val: Maybe): Either;
-declare function maybeToEither(fn: UnaryFunction<Maybe>): UnaryFunction<Either>;
+ declare function maybeToEither(errVal: any): (maybe: Maybe) => Either;
+ declare function maybeToEither(errVal: any, maybe: Maybe): Either;
+ declare function maybeToEither(fn: UnaryFunction<Maybe>): (maybe: Maybe) => UnaryFunction<Either>;
+ declare function maybeToEither(fn: UnaryFunction<Maybe>, maybe: Maybe): UnaryFunction<Either>;
 
 export default maybeToEither;
