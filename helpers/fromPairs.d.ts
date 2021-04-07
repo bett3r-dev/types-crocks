3f7f1fdca159d0c5839cc6763c15ac0d4d5d7c73
Tomás Ruiz <tomasruizr@gmail.com>
2021-04-07 20:55:10 -0300

    [add] helpers refactor

--- helpers/fromPairs.d.ts
+++ helpers/fromPairs.d.ts
@@ -4,6 +4,6 @@ import Pair from '../Pair';
 /**
  * fromPairs :: Foldable f => f (Pair String a) -> Object
  */
-declare function fromPairs(val: Array<Pair> | List): object;
+declare function fromPairs<F,S=F>(val: Array<Pair<F,S>> | List): object;
 
 export default fromPairs;
