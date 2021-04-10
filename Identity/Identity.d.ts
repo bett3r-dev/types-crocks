790682f004ed0ddc0291169f056c1578b37b57b8
Tomás Ruiz <tomasruizr@gmail.com>
2021-04-10 17:10:51 -0300

    [add] Identity refactor

--- Identity/Identity.d.ts
+++ Identity/Identity.d.ts
@@ -1,17 +1,18 @@
 import { UnaryFunction } from '../internal/index';
 
-declare function Identity(val: unknown): Identity;
+declare function Identity<T>(val: T): Identity<T>;
 
-declare class Identity {
-    equals(val: unknown): boolean;
-    concat(val: Identity): Identity;
-    map(fn: UnaryFunction): Identity;
-    ap(val: Identity): Identity;
+declare class Identity<TYPE> {
+    equals<T=TYPE>(val: T): boolean;
+    concat<T=TYPE>(val: Identity<T>): Identity<T>;
+    map<T=TYPE>(fn: UnaryFunction<T>): Identity<T>;
+    chain<T=TYPE>(fn: UnaryFunction<Identity<T>, T>): Identity<T>;
+    ap<T=TYPE>(val: Identity<T>): Identity<T>;
     sequence(val: unknown): any;
     traverse(val: unknown): any;
-    chain(fn: UnaryFunction<Identity>): Identity;
-    valueOf(): any;
-    static of(val: unknown): Identity;
+    valueOf<T=TYPE>(): T;
+    type():string;
+    static of<T>(val: T): Identity<T>;
 }
 
 export default Identity;
