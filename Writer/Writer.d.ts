ecbf6755c0451c3f2a602c52f22667db0dc531c9
Tomás Ruiz <tomasruizr@gmail.com>
2021-05-30 13:45:55 -0300

    [update] Writer.of

--- Writer/Writer.d.ts
+++ Writer/Writer.d.ts
@@ -1,19 +1,20 @@
 import { Applicative, Functor, Monad, UnaryFunction, Monoid } from "crocks/internal";
 import Pair from "crocks/Pair";
 
-declare function Writer<T>(monoid: Monoid<T>): Writer<T, Monoid<T>>;
+declare function Writer<T, M>(monoid: Monoid<M>): Writer<T, Monoid<M>>;
 
-declare class Writer<T, M extends Monoid<T>> implements Functor<T>, Monad<T>, Applicative<T> {
+declare class Writer<T, M> implements Functor<T>, Monad<T>, Applicative<T> {
     map<RETURN=T>(fn: UnaryFunction<RETURN,T>): Writer<T,M>;
-    chain<RETURN=T>(fn: UnaryFunction<Writer<RETURN,Monoid<RETURN>>, T>): Writer<RETURN,Monoid<RETURN>>;
+    chain<RETURN=T>(fn: UnaryFunction<Writer<RETURN,Monoid<M>>, T>): Writer<RETURN,Monoid<M>>;
     ap(val: Writer<T,M>): Writer<T,M>;
     equals: (otherWriter: Writer<T,M>) => boolean
-    log: () => M
-    read: () => Pair<M,T>
+    of: <T>(value:T) => Writer<T,M>
+    log: () => Monoid<M>
+    read: () => Pair<Monoid<M>,T>
     valueOf(): boolean;
     type():string;
 
-    static of<T>(val: T): Writer<T, Monoid<T>>;
+    static of<T, M>(val: T): Writer<T, Monoid<M>>;
 }
 
 export default Writer;
